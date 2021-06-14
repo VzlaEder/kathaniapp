4a902fbeeb1e9ac206548b3fc88a7224273caa18
VzlaEder <eder.bm2014@gmail.com>
2021-06-13 23:11:45 -0400

    errores heroku corregidos

--- src/app/pages/welcome/welcome-routing.module.ts
+++ src/app/pages/welcome/welcome-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { WelcomePage } from './welcome.page';
-import { Noauthenticated } from '../../guards/noauthenticated.guard';
+import { Noauthenticated } from '../../guards/Noauthenticated.guard';
 
 
 const routes: Routes = [
